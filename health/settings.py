66ed9d7b262f3c6e6bbcfa105c911f6d20a724fd
user <user@users-MacBook-Air.local>
2022-08-03 10:50:58 -0400

    changed a lot

--- health/settings.py
+++ health/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m18(c_)xwe5!*)l_$9o)xg1-fo5txila)05dyaqjaqf^)^$exw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
